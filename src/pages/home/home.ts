ef2a82ce353e9f764a985e38c6d4a04a8e4413b2
Daskus <bertagnolli.daniel@yahoo.de>
2017-04-02 13:43:12 +0200

    initial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { NavController, IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  private values: Array<number> = [];
+
+  constructor(public navCtrl: NavController) {
+    for(let i=0; i<200; i++){
+      this.values.push(i);
+    }
+  }
+
+}
